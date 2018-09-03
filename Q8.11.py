2e957e979078720aa0a048f639cecab840a88398
kevincott97@gmail.com <kevincott97@gmail.com>
2018-09-02 23:37:12 -0400

    minor changes

--- Q8.11.py
+++ Q8.11.py
@@ -1,4 +1,9 @@
-def coins (amount, denominations,  curr_denom):
+# Store the number of ways to make change for a given amount and denomination
+
+def coins (amount, denominations,  curr_denom, memory):
+    if (memory[amount][curr_denom] > 0):
+        return memory[amount][curr_denom]
+
     # Only one way to make change with pennies
     if (curr_denom >= len(denominations) - 1):
         return 1
@@ -15,11 +20,14 @@ def coins (amount, denominations,  curr_denom):
     i = 0
     ways = 0
     while (i * denom <= amount):
-        ways += coins(amount - i * denom, denominations, curr_denom + 1)
+        ways += coins(amount - i * denom, denominations, curr_denom + 1, memory)
         i += 1
+    
+    memory[amount][curr_denom] = ways
     return ways
 
 def get_change(n):
-    return coins(n, [25, 10, 5, 1], 0)
+    memory = [[0 for x in range(4)] for y in range(n + 1)]
+    return coins(n, [25, 10, 5, 1], 0, memory)
 
 print(get_change(10))
